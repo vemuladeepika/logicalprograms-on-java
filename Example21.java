859f1620cf9c565a9d7491aa98046b718a34643d
vemuladeepika <96418073+vemuladeepika@users.noreply.github.com>
2022-03-09 14:55:36 +0530

    Add files via upload

---
+++ Example21.java
@@ -0,0 +1,9 @@
+class Example21
+{
+    public static void main(String args[])
+    {
+        int si,p=800,q=9,r=7;
+        si=(p*q*r/100);
+        System.out.println("simple intrest ="+si);
+    }
+}
