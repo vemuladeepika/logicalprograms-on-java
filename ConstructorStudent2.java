1e4f83a114f72b7c07f63ee11fc3c42128022961
vemuladeepika <96418073+vemuladeepika@users.noreply.github.com>
2022-03-14 23:52:53 +0530

    Add files via upload

---
+++ ConstructorStudent2.java
@@ -0,0 +1,73 @@
+import java.util.*;
+class Student
+{
+    int rollno;
+    String name;
+    String section;
+    int marks;
+    String address;
+    //constructor overloading
+    Student(int rollno,String name,String section,int marks,String address)
+    {
+        this.rollno=rollno;
+        this.name=name;
+        this.section=section;
+        this.marks=marks;
+        this.address=address;
+        
+    }
+     
+    Student()
+    {
+       // this.Student(123,"deepika","A",50,"sircilla");
+    }
+    Student(int r)
+    {
+        rollno=r;
+    }
+    
+    
+    void display()
+    {
+        System.out.println(rollno+"\n"+name+"\n"+section+"\n"+marks+"\n"+address);
+    }
+    
+    void reading()
+    {
+        System.out.println(name+" reading");
+    }
+    void writing()
+    {
+        this.reading();
+        System.out.println(name+" Writing");
+    }
+    
+    
+}
+class ConstructorStudent2
+{
+    public static void main(String args[])
+    {
+        Student std1=new Student(123,"deepika","A",90,"sircilla");
+     
+      
+        System.out.println("Student 1 informatio");
+        std1.display();
+        std1.reading();
+        std1.writing();
+        Student std2=new Student(345);
+      
+        System.out.println("Student 2 informatio");
+      std2.display();
+        std2.reading();
+        std2.writing();
+         Student std3=new Student();
+      
+        System.out.println("Student 3 informatio");
+      std3.display();
+        std3.reading();
+        std3.writing();
+
+        
+    }
+}
