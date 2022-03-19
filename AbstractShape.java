13de12f5265e8bb7e43b06fcb623f0070a92affb
vemuladeepika <96418073+vemuladeepika@users.noreply.github.com>
2022-03-20 01:19:44 +0530

    Add files via upload

---
+++ AbstractShape.java
@@ -0,0 +1,31 @@
+abstract class Shape{
+    abstract void draw();
+}
+class Circle extends Shape{
+    void draw()
+    {
+        System.out.println("circle");
+    }
+}
+class Square extends Shape
+{
+    void draw()
+    {
+        System.out.println("Square");
+    }
+}
+class Rectangle extends Shape{
+    void draw()
+    {
+        System.out.println("Rectangle");
+    }
+}
+class AbstractShape
+{
+    public static void main(String args[])
+    {
+        Shape s=new Circle();
+        s.draw();
+    }
+    
+}
